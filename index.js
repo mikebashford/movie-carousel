02d041f975278c5f13cbd274c1d24f00f78163c6
Michael Bashford <mikebashford@gmail.com>
2022-07-15 18:38:04 -0500

    Fixed scrolling

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ const container = document.querySelector(".carousel-container");
 leftButton.addEventListener("click", () => {
   container.scrollBy({
     top: 0,
-    right: +100,
+    left: -100,
     behavior: "smooth",
   });
 });
@@ -107,7 +107,7 @@ leftButton.addEventListener("click", () => {
 rightButton.addEventListener("click", () => {
   container.scrollBy({
     top: 0,
-    left: +100,
+    left: 100,
     behavior: "smooth",
   });
 });
